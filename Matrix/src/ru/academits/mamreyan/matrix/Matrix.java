d0de5c02de6f67cbaa003061b26183b7d69d677e
DavidMamreyan <david.008@bk.ru>
2021-03-02 04:19:53 +0700

    Задача Matrix готова к проверке. Замечания, относящиеся к Vector учтены.

--- Matrix/src/ru/academits/mamreyan/matrix/Matrix.java
+++ Matrix/src/ru/academits/mamreyan/matrix/Matrix.java
@@ -7,35 +7,33 @@ import java.util.Arrays;
 public class Matrix {
     private int n;
     private int m;
-    private Vector[] vectorsArray;
+    private Vector[] vectors;
 
     public Matrix(int n, int m) {
         this.n = n;
         this.m = m;
-        vectorsArray = new Vector[n];
+        vectors = new Vector[n];
 
         for (int i = 0; i < n; i++) {
-            vectorsArray[i] = new Vector(m);
+            vectors[i] = new Vector(m);
         }
     }
 
     public Matrix(Matrix matrix) {
         n = matrix.n;
         m = matrix.m;
-        vectorsArray = new Vector[n];
+        vectors = new Vector[n];
 
-        for (int i = 0; i < n; i++) {
-            vectorsArray[i] = new Vector(matrix.vectorsArray[i]);
-        }
+        vectors = Arrays.copyOf(matrix.vectors, n);
     }
 
-    public Matrix(double[][] vectorsArray) {
-        n = vectorsArray.length;
-        this.vectorsArray = new Vector[n];
+    public Matrix(double[][] vectors) {
+        n = vectors.length;
+        this.vectors = new Vector[n];
 
         int maxM = 0;
 
-        for (double[] doubles : vectorsArray) {
+        for (double[] doubles : vectors) {
             if (doubles.length > maxM) {
                 maxM = doubles.length;
             }
@@ -43,30 +41,29 @@ public class Matrix {
 
         m = maxM;
 
-
         for (int i = 0; i < n; i++) {
-            this.vectorsArray[i] = new Vector(vectorsArray[i]);
-            this.vectorsArray[i].appendZeroComponents(m - vectorsArray[i].length);
+            this.vectors[i] = new Vector(vectors[i]);
+            this.vectors[i].appendZeroComponents(m - vectors[i].length);
         }
     }
 
-    public Matrix(Vector[] vectorsArray) {
-        n = vectorsArray.length;
-        this.vectorsArray = new Vector[n];
+    public Matrix(Vector[] vectors) {
+        n = vectors.length;
+        this.vectors = new Vector[n];
 
         int maxM = 0;
 
         for (int i = 0; i < n; i++) {
-            if (vectorsArray[i].getSize() > maxM) {
-                maxM = vectorsArray[i].getSize();
+            if (vectors[i].getSize() > maxM) {
+                maxM = vectors[i].getSize();
             }
         }
 
         m = maxM;
 
         for (int i = 0; i < n; i++) {
-            this.vectorsArray[i] = new Vector(vectorsArray[i]);
-            this.vectorsArray[i].appendZeroComponents(m - vectorsArray[i].getSize());
+            this.vectors[i] = new Vector(vectors[i]);
+            this.vectors[i].appendZeroComponents(m - vectors[i].getSize());
         }
     }
 
@@ -78,47 +75,48 @@ public class Matrix {
         return m;
     }
 
-    public Vector getVectorByIndex(int i) {
-        if (i < 0 || i >= n) {
-            throw new IllegalArgumentException("i must me 0 <= i < n");
+    public Vector getVectorByIndex(int index) {
+        if (index < 0 || index >= n) {
+            throw new IndexOutOfBoundsException("index must me 0 <= index < n. index = " + index + ", n = " + n);
         }
 
-        return vectorsArray[i];
+        return vectors[index];
     }
 
-    public void setVectorByIndex(int i, Vector vector) {
+    public void setVectorByIndex(int index, Vector vector) {
         if (vector.getSize() != m) {
-            throw new IllegalArgumentException("vector.n is not equal to matrix.m");
+            throw new IllegalArgumentException("vector's length must be equal to m. vector's length = " + vector.getSize() +
+                    ", m = " + m);
         }
 
-        vectorsArray[i] = new Vector(vector);
+        vectors[index] = new Vector(vector);
     }
 
-    public Vector getColumnByIndex(int j) {
-        if (j < 0 || j >= m) {
-            throw new IllegalArgumentException("i must me 0 <= i < m");
+    public Vector getColumnByIndex(int index) {
+        if (index < 0 || index >= m) {
+            throw new IndexOutOfBoundsException("index must me 0 <= index < m. index = " + index + ", m = " + m);
         }
 
         Vector vector = new Vector(n);
 
         for (int i = 0; i < n; i++) {
-            vector.setComponentByIndex(i, vectorsArray[i].getComponentByIndex(j));
+            vector.setComponentByIndex(i, vectors[i].getComponentByIndex(index));
         }
 
         return vector;
     }
 
     @Override
-    public final String toString() {
-        StringBuilder line = new StringBuilder("{");
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder("{");
 
         for (int i = 0; i < n; i++) {
-            line.append(vectorsArray[i]).append(", ");
+            stringBuilder.append(vectors[i]).append(", ");
         }
 
-        line.replace(line.length() - 2, line.length() - 1, "}");
+        stringBuilder.replace(stringBuilder.length() - 2, stringBuilder.length() - 1, "}");
 
-        return line.toString();
+        return stringBuilder.toString();
     }
 
     @Override
@@ -132,7 +130,7 @@ public class Matrix {
         }
 
         Matrix matrix = (Matrix) o;
-        return n == matrix.n && m == matrix.m && Arrays.equals(vectorsArray, matrix.vectorsArray);
+        return n == matrix.n && m == matrix.m && Arrays.equals(vectors, matrix.vectors);
     }
 
     @Override
@@ -141,8 +139,36 @@ public class Matrix {
         int hash = 1;
         hash = (prime * hash) + n;
         hash = (prime * hash) + m;
-        hash = (prime * hash) + Arrays.hashCode(vectorsArray);
+        hash = (prime * hash) + Arrays.hashCode(vectors);
         return hash;
+    }
+
+    public Matrix add(Matrix matrix) {
+        if (n != matrix.n || m != matrix.m) {
+            throw new IllegalArgumentException("matrices' dimensions are not equal. n1 = " + n + ", m1 = " + m +
+                    ", n2 = " + matrix.n + ", m2 = " + matrix.m);
+        }
+
+        for (int i = 0; i < n; i++) {
+            vectors[i].add(matrix.vectors[i]);
+        }
+
+
+        return this;
+    }
+
+    public Matrix subtract(Matrix matrix) {
+        if (n != matrix.n || m != matrix.m) {
+            throw new IllegalArgumentException("matrices' dimensions are not equal. n1 = " + n + ", m1 = " + m +
+                    ", n2 = " + matrix.n + ", m2 = " + matrix.m);
+        }
+
+        for (int i = 0; i < n; i++) {
+            vectors[i].subtract(matrix.vectors[i]);
+        }
+
+
+        return this;
     }
 
     public Matrix transpose() {
@@ -150,169 +176,141 @@ public class Matrix {
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                tempMatrix.vectorsArray[i].setComponentByIndex(j, vectorsArray[j].getComponentByIndex(i));
+                tempMatrix.vectors[i].setComponentByIndex(j, vectors[j].getComponentByIndex(i));
             }
         }
 
         n = tempMatrix.n;
         m = tempMatrix.m;
-        vectorsArray = new Vector[n];
-
-        for (int i = 0; i < n; i++) {
-            vectorsArray[i] = new Vector(tempMatrix.vectorsArray[i]);
-        }
+        vectors = tempMatrix.vectors;
 
         return this;
     }
 
-    public Matrix multiplyByScalar(double n) {
-        for (int i = 0; i < n; i++) {
-            vectorsArray[i] = vectorsArray[i].multiplyVector(n);
+    public Matrix multiplyByScalar(double scalar) {
+        for (int i = 0; i < scalar; i++) {
+            vectors[i].multiplyByScalar(scalar);
         }
 
         return this;
-    }
-
-    private Matrix getMatrixWithCrossedOutStrAndCol(@SuppressWarnings("SameParameterValue") int k, int l) {
-        if (k >= n || l >= m) {
-            throw new IllegalArgumentException("k must be 0 <= k < n and l must be 0 <= l <= m");
-        }
-
-        Matrix matrix = new Matrix(n - 1, m - 1);
-
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < m - 1; j++) {
-                if (i >= k) {
-                    if (j >= l) {
-                        matrix.vectorsArray[i].setComponentByIndex(j, vectorsArray[i + 1].getComponentByIndex(j + 1));
-                    } else {
-                        matrix.vectorsArray[i].setComponentByIndex(j, vectorsArray[i + 1].getComponentByIndex(j));
-                    }
-                } else if (j >= l) {
-                    matrix.vectorsArray[i].setComponentByIndex(j, vectorsArray[i].getComponentByIndex(j + 1));
-                } else {
-                    matrix.vectorsArray[i].setComponentByIndex(j, vectorsArray[i].getComponentByIndex(j));
-
-                }
-            }
-        }
-
-        return matrix;
-    }
-
-    public double getMatrixDeterminant() {
-        if (n != m) {
-            throw new IllegalArgumentException("matrix dimensions must be equal");
-        }
-
-        if (n == 1) {
-            return vectorsArray[0].getComponentByIndex(0);
-        }
-
-        if (n == 2) {
-            return vectorsArray[0].getComponentByIndex(0) * vectorsArray[1].getComponentByIndex(1) -
-                    vectorsArray[1].getComponentByIndex(0) * vectorsArray[0].getComponentByIndex(1);
-        }
-
-        double determinant = 0;
-
-        for (int i = 0; i < m; i++) {
-            determinant += Math.pow(-1, 1 + (i + 1)) * vectorsArray[0].getComponentByIndex(i) *
-                    this.getMatrixWithCrossedOutStrAndCol(0, i).getMatrixDeterminant();
-        }
-
-        return determinant;
     }
 
     public Matrix multiplyByVector(Vector vector) {
         Matrix matrix;
 
         if (n == vector.getSize()) {
-            matrix = new Matrix(Matrix.multiplication(this, new Matrix(new Vector[]{vector})));
+            matrix = new Matrix(Matrix.getMultiplication(this, new Matrix(new Vector[]{vector})));
         } else if (m == vector.getSize()) {
-            matrix = new Matrix(Matrix.multiplication(this, new Matrix(new Vector[]{vector}).transpose()));
+            matrix = new Matrix(Matrix.getMultiplication(this, new Matrix(new Vector[]{vector}).transpose()));
         } else {
-            throw new IllegalArgumentException("matrix n or m must be equal to vector.n");
+            throw new IllegalArgumentException("vector's length must be equal to matrix's n or m. vector's length = " + vector.getSize() +
+                    ", n = " + n + ", m =" + m);
         }
 
         n = matrix.n;
         m = matrix.m;
-        vectorsArray = new Vector[n];
-
-        System.arraycopy(matrix.vectorsArray, 0, vectorsArray, 0, n);
+        vectors = matrix.vectors;
 
         return this;
     }
 
-    public Matrix addMatrix(Matrix matrix) {
-        if (n != matrix.n || m != matrix.m) {
-            throw new IllegalArgumentException("matrices' dimensions are not equal");
+    public double getMatrixDeterminant() {
+        if (n != m) {
+            throw new IllegalArgumentException("matrix's dimensions must be equal. n = " + n + ", m = " + m);
         }
 
-        for (int i = 0; i < n; i++) {
-            vectorsArray[i] = Vector.addition(vectorsArray[i], matrix.vectorsArray[i]);
+        if (n == 1) {
+            return vectors[0].getComponentByIndex(0);
         }
 
+        if (n == 2) {
+            return vectors[0].getComponentByIndex(0) * vectors[1].getComponentByIndex(1) -
+                    vectors[1].getComponentByIndex(0) * vectors[0].getComponentByIndex(1);
+        }
 
-        return this;
+        double determinant = 0;
+
+        for (int i = 0; i < m; i++) {
+            determinant += Math.pow(-1, 1 + (i + 1)) * vectors[0].getComponentByIndex(i) *
+                    this.getMatrixWithCrossedOutStrAndCol(0, i).getMatrixDeterminant();
+        }
+
+        return determinant;
     }
 
-    public Matrix subtractMatrix(Matrix matrix) {
-        if (n != matrix.n || m != matrix.m) {
-            throw new IllegalArgumentException("matrices' dimensions are not equal");
+    private Matrix getMatrixWithCrossedOutStrAndCol(@SuppressWarnings("SameParameterValue") int k, int l) {
+        if (k < 0 || k >= n || l < 0 || l >= m) {
+            throw new IndexOutOfBoundsException("k must be 0 <= k < n and l must be 0 <= l <= m. k = " + k + ", l = " + l +
+                    ", n = " + n + ", m = " + m);
         }
 
-        for (int i = 0; i < n; i++) {
-            vectorsArray[i] = Vector.subtraction(vectorsArray[i], matrix.vectorsArray[i]);
-        }
+        Matrix matrix = new Matrix(n - 1, m - 1);
 
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < l; j++) {
+                matrix.vectors[i].setComponentByIndex(j, vectors[i].getComponentByIndex(j));
+            }
 
-        return this;
-    }
-
-    public static Matrix addition(Matrix matrix1, Matrix matrix2) {
-        if (matrix1.n != matrix2.n || matrix1.m != matrix2.m) {
-            throw new IllegalArgumentException("matrices' dimensions are not equal");
-        }
-
-        Matrix resultMatrix = new Matrix(matrix1.n, matrix1.m);
-
-        for (int i = 0; i < resultMatrix.n; i++) {
-            resultMatrix.vectorsArray[i] = Vector.addition(matrix1.vectorsArray[i], matrix2.vectorsArray[i]);
-        }
-
-        return resultMatrix;
-    }
-
-    public static Matrix subtraction(Matrix matrix1, Matrix matrix2) {
-        if (matrix1.n != matrix2.n || matrix1.m != matrix2.m) {
-            throw new IllegalArgumentException("matrices' dimensions are not equal");
-        }
-
-        Matrix resultMatrix = new Matrix(matrix1.n, matrix1.m);
-
-        for (int i = 0; i < resultMatrix.n; i++) {
-            resultMatrix.vectorsArray[i] = Vector.subtraction(matrix1.vectorsArray[i], matrix2.vectorsArray[i]);
-        }
-
-        return resultMatrix;
-    }
-
-    public static Matrix multiplication(Matrix matrix1, Matrix matrix2) {
-        if (matrix1.m != matrix2.n) {
-            throw new IllegalArgumentException("matrices' dimensions are not proper. matrix1.m must me equal to matrix2.n");
-        }
-
-        Matrix resultMatrix = new Matrix(matrix1.n, matrix2.m);
-        matrix2.transpose();
-
-        for (int i = 0; i < resultMatrix.n; i++) {
-            for (int j = 0; j < resultMatrix.m; j++) {
-                resultMatrix.vectorsArray[i].setComponentByIndex(j, Vector.scalarProduct(matrix1.vectorsArray[i], matrix2.vectorsArray[j]));
+            for (int j = l + 1; j < m; j++) {
+                matrix.vectors[i].setComponentByIndex(j - 1, vectors[i].getComponentByIndex(j));
             }
         }
 
-        matrix2.transpose();
+        for (int i = k + 1; i < n; i++) {
+            for (int j = 0; j < l; j++) {
+                matrix.vectors[i - 1].setComponentByIndex(j, vectors[i].getComponentByIndex(j));
+            }
+
+            for (int j = l + 1; j < m; j++) {
+                matrix.vectors[i - 1].setComponentByIndex(j - 1, vectors[i].getComponentByIndex(j));
+            }
+        }
+
+        return matrix;
+    }
+
+    public static Matrix getSum(Matrix matrix1, Matrix matrix2) {
+        if (matrix1.n != matrix2.n || matrix1.m != matrix2.m) {
+            throw new IllegalArgumentException("matrices' dimensions are not equal. n1 = " + matrix1.n + ", m1 = " + matrix1.m +
+                    ", n2 = " + matrix2.n + ", m2 = " + matrix2.m);
+        }
+
+        Matrix resultMatrix;
+        Matrix tempMatrix = new Matrix(matrix1);
+
+        resultMatrix = tempMatrix.add(matrix2);
+
+        return resultMatrix;
+    }
+
+    public static Matrix getDifference(Matrix matrix1, Matrix matrix2) {
+        if (matrix1.n != matrix2.n || matrix1.m != matrix2.m) {
+            throw new IllegalArgumentException("matrices' dimensions are not equal");
+        }
+
+        Matrix resultMatrix;
+        Matrix tempMatrix = new Matrix(matrix1);
+
+        resultMatrix = tempMatrix.subtract(matrix2);
+
+        return resultMatrix;
+    }
+
+    public static Matrix getMultiplication(Matrix matrix1, Matrix matrix2) {
+        if (matrix1.m != matrix2.n) {
+            throw new IllegalArgumentException("matrices' dimensions are not proper. matrix1.m must me equal to matrix2.n. " +
+                    "matrix1.m = " + matrix1.m + ", matrix2.n = " + matrix2.n);
+        }
+
+        Matrix resultMatrix = new Matrix(matrix1.n, matrix2.m);
+        Matrix tempMatrix = new Matrix(matrix2);
+        tempMatrix.transpose();
+
+        for (int i = 0; i < resultMatrix.n; i++) {
+            for (int j = 0; j < resultMatrix.m; j++) {
+                resultMatrix.vectors[i].setComponentByIndex(j, Vector.getScalarProduct(matrix1.vectors[i], tempMatrix.vectors[j]));
+            }
+        }
 
         return resultMatrix;
     }
--- Matrix/src/ru/academits/mamreyan/matrix_main/Main.java
+++ Matrix/src/ru/academits/mamreyan/matrix_main/Main.java
@@ -5,40 +5,37 @@ import ru.academits.mamreyan.vector.Vector;
 
 public class Main {
     public static void main(String[] args) {
-        Matrix matrix1 = new Matrix(2, 4);
-        Matrix matrix2 = new Matrix(new Vector[]{new Vector(new double[]{1}), new Vector(new double[]{1, 2}), new Vector(new double[]{1, 2, 3})});
+        Matrix matrix1 = new Matrix(new Vector[]{
+                new Vector(new double[]{1}),
+                new Vector(new double[]{1, 2}),
+                new Vector(new double[]{1, 2, 3})
+        });
 
+        System.out.println(matrix1);
         System.out.println(matrix1.getVectorByIndex(1));
-        System.out.println(matrix2);
-        matrix2.setVectorByIndex(1, new Vector(new double[]{10, 20, 30}));
-        System.out.println(matrix2);
+        System.out.println(matrix1.getColumnByIndex(1));
+        matrix1.setVectorByIndex(1, new Vector(new double[]{10, 20, 30}));
+        System.out.println(matrix1);
         System.out.println(matrix1.getN());
-        System.out.println(matrix2.getM());
+        System.out.println(matrix1.getM());
 
         System.out.println("__________________");
 
-        matrix1 = new Matrix(new double[][]{{1, 2}, {3, 4}, {5, 6}});
+        matrix1 = new Matrix(new double[][]{{1, 2}, {3, 4}});
+        Matrix matrix2 = new Matrix(new double[][]{{4, 3}, {2, 1}});
 
         System.out.println(matrix1);
+        System.out.println(matrix2);
+        System.out.println(Matrix.getSum(matrix1, matrix2));
+
+        System.out.println("__________________");
+
+        matrix1 = new Matrix(new double[][]{{1, 2}, {3, 4}});
+        matrix2 = new Matrix(matrix1.transpose());
+
         System.out.println(matrix1.transpose());
-
-        System.out.println("__________________");
-
-        matrix1 = new Matrix(new double[][]{{1, 2}, {3, 4}});
-        matrix2 = new Matrix(new double[][]{{4, 3}, {2, 1}});
-
-        System.out.println(matrix1);
         System.out.println(matrix2);
-        System.out.println(Matrix.addition(matrix1, matrix2));
-
-        System.out.println("__________________");
-
-        matrix1 = new Matrix(new double[][]{{1, 2}, {3, 4}});
-        matrix2 = new Matrix(matrix1);
-
-        System.out.println(matrix1);
-        System.out.println(matrix2);
-        System.out.println(Matrix.subtraction(matrix1, matrix2));
+        System.out.println(Matrix.getDifference(matrix1, matrix2));
 
         System.out.println("__________________");
 
@@ -47,13 +44,7 @@ public class Main {
 
         System.out.println(matrix1);
         System.out.println(matrix2);
-        System.out.println(Matrix.multiplication(matrix1, matrix2));
-
-        System.out.println("__________________");
-
-        matrix1 = new Matrix(new double[][]{{1, 2}, {3, 4}, {5, 6}});
-
-        System.out.println(matrix1.getColumnByIndex(0));
+        System.out.println(Matrix.getMultiplication(matrix1, matrix2));
 
         System.out.println("__________________");
 
@@ -100,7 +91,7 @@ public class Main {
 
         System.out.println(matrix1);
         System.out.println(matrix2);
-        System.out.println(matrix1.addMatrix(matrix2));
+        System.out.println(matrix1.add(matrix2));
 
         System.out.println("__________________");
 
@@ -109,6 +100,6 @@ public class Main {
 
         System.out.println(matrix1);
         System.out.println(matrix2);
-        System.out.println(matrix1.subtractMatrix(matrix2));
+        System.out.println(matrix1.subtract(matrix2));
     }
 }
